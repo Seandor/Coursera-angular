aa88f13d6ec326f4d3ed3d8b443dac9ba62934b3
Seandor <seandor123@gmail.com>
2016-07-15 23:35:55 +0800

    ng-route learning...

--- spa/conFusion-copy/app/scripts/app.js
+++ spa/conFusion-copy/app/scripts/app.js
@@ -1,3 +1,23 @@
-angular.module('confusionApp', [])
+angular.module('confusionApp', ['ngRoute'])
+	.config (function($routeProvider) {
+		'use strict';
 
+		$routeProvider
+		// route for the contactus page
+		.when('/contactus', {
+			templateUrl: 'templates/contactus.html',
+			controller: 'ContactController'
+		})
+		// route for the menu page
+		.when('/menu', {
+			templateUrl: 'templates/menu.html',
+			controller: 'MenuController'
+		})
+		// route for the dish details page
+		.when('/menu/:id', {
+			templateUrl: 'templates/dishdetail.html',
+			controller: 'DishDetailController'
+		})
+		.otherwise('/contactus');
+	})
 ;
